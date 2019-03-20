5802c1de42a0999cb9978ffd0f646cf11b352595
Christian Medina <christian.medina@csu.fullerton.edu>
2019-03-20 11:59:13 -0700

    Delete config.js

--- dist/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = {
-    "token": "NTU3OTg3OTgyNjAzNDUyNDQw.D3QXeA.kDU7ymA56v0_O_kxVzgPOX-byjY",
-    "prefix": "?",
-};
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vc3JjL2NvbmZpZy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFXLFFBQUEsTUFBTSxHQUFDO0lBQ2QsT0FBTyxFQUFFLDZEQUE2RDtJQUN0RSxRQUFRLEVBQUUsR0FBRztDQUNoQixDQUFBIn0=
